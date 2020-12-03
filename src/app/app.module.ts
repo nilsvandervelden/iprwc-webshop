272f2d20d3cf49a97d2a4fe1f983853de0e97a81
Nils van der Velden <nilsepils123@gmail.com>
2020-12-03 13:15:33 +0100

    File and modal setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ProductEditComponent } from './products/product-edit/product-edit.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductItemComponent } from './products/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductDetailComponent,
+    ProductEditComponent,
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule
