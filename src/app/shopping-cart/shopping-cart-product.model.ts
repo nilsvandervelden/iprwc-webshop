296eec25eb53029cd62739e364aafadadce79c2e
Nils van der Velden <nilsepils123@gmail.com>
2020-12-12 15:38:53 +0100

    Styling shopping cart

--- src/app/shopping-cart/shopping-cart-product.model.ts
+++ src/app/shopping-cart/shopping-cart-product.model.ts
@@ -2,14 +2,14 @@ import { Product } from '../products/product-model';
 
 export class shoppingListItem {
   id: number;
-  productId: number;
+  vinylFigureId: number;
   productName: string;
   price: number;
   quantity: number;
 
 constructor(id: number, product: Product, quantity = 1) {
   this.id = id;
-  this.productId = product.id;
+  this.vinylFigureId = product.vinylFigureId;
   this.productName = product.name;
   this.price = product.price;
   this.quantity = quantity;
