c5e2dfbf4ef60f532247ea011f1bf5f6c93061b4
Nils van der Velden <nilsepils123@gmail.com>
2020-12-21 15:12:25 +0100

    Can now add products to shopping list from productlist

--- src/app/products/product-list/product-item/product-item.component.ts
+++ src/app/products/product-list/product-item/product-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ShoppingCartService } from 'src/app/shopping-cart/shopping-cart.service';
 import { Product } from '../../product-model';
 import { ProductService } from '../../product.service';
 
@@ -12,7 +13,14 @@ export class ProductItemComponent implements OnInit {
   @Input() product: Product;
   @Input() index: number;
 
+  constructor(private shoppingCartService: ShoppingCartService){}
+
   ngOnInit(): void {
 
+  }
+
+  onAddToCart() {
+    console.log(this.product.name);
+    this.shoppingCartService.onAddItemToShoppingList(this.product)
   }
 }
