7b87a3dad864142173fa42ee22173718157d1183
Nils van der Velden <nilsepils123@gmail.com>
2020-12-23 18:12:21 +0100

    FIXED IT@!!!!!

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const Product = require("../models/product");
 
-const router = exporess.Router();
+const router = express.Router();
 
 router.post("", (req, res, next ) => {
   const product = new Product({
@@ -21,7 +21,6 @@ router.post("", (req, res, next ) => {
 });
 
 router.put("/:id", (req, res, next) => {
-  console.log(req.body);
   const product = new Product({
     _id: req.body.id,
     vinylFigureId: req.body.vinylFigureId,
@@ -31,30 +30,28 @@ router.put("/:id", (req, res, next) => {
     imagePath: req.body.imagePath
   });
   Product.updateOne({ _id: req.params.id }, product).then(result => {
-    console.log(result);
     res.status(200).json({message: "Update successful!" });
   });
 });
 
 router.get("", (req, res, next) => {
-  Product.find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'Products fetched successfully!',
-        products: documents
-      });
+  Product.find().then(documents => {
+    res.status(200).json({
+      message: 'Products fetched successfully!',
+      products: documents
     });
+  });
 });
 
-router.get("/:id"), (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   Product.findById(req.params.id).then(product => {
     if (product) {
       res.status(200).json(product)
     } else {
       res.status(404).json({message: 'Product not found!'});
     }
-  })
-}
+  });
+});
 
 router.delete("/:id", (req, res, next) => {
   Product.deleteOne({_id: req.params.id}).then(result => {
