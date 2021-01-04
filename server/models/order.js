ddb0796217cd8ae945c81bc3e9445522abb4cd4a
Nils van der Velden <nilsepils123@gmail.com>
2021-01-04 11:41:10 +0100

    Added backend models

---
+++ server/models/order.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const productOrderSchema = require('./productOrder')
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  products: {
+    type: [productOrderSchema]
+    required: true,
+    unique: false
+  },
+  userId {
+    type: String,
+    required: true
+  }
+  createdAt: {
+    type: Date,
+    required: true
+  }
+  paid: {
+    type: Boolean,
+    default: false
+  },
+  delivered: {
+    type: Boolean,
+    default: false
+  }
+})
+
+module.exports = mongoose.model('Order', orderSchema);
--- server/models/productOrder.js
+++ server/models/productOrder.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const productSchema = mongoose.Schema({
+const productOrderSchema = new Schema({
   vinylFigureId: {type: Number, required: true},
   name:  {type: String, required: true},
   price:  {type: Number, required: true},
   description: {type: String, required: true},
   imagePath: {type: String, required: true},
-})
+  amount: {type: Number, required: true}
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model('Order', productOrderSchema);
