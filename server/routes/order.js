5f784f74b85cfb24afd305bc8ac2b7bb5285205e
Nils van der Velden <nilsepils123@gmail.com>
2021-01-05 16:23:17 +0100

    reworking whole order backend

--- server/routes/order.js
+++ server/routes/order.js
@@ -7,21 +7,25 @@ const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
-router.get("", async (req, res, next) => {
+router.get("/:id", checkAuth, async (req, res, next) => {
   try {
-    const product = await global.models('Order').find().sort([['createdAt', -1]])
-    res.json(await product)
+    const order = await Order.findById(req.params.id)
+    if (req.user.id !== order.userId) return res.status(401).json({success: false, message: 'Thats not your order'})
+    const user = await User.findById(order.userId)
+    res.json({
+      user, order
+    })
   } catch (e) {
-    res.send({ success: false, message: 'couldnt fetch order' })
+    res.status(400).json({succes: false, message: 'couldnt fetch order' })
   }
-});
+})
 
 router.get("/:id", (req, res, next) => {
-  ProductOrder.findById(req.params.id).then(order => {
-    if (order) {
-      res.status(200).json(order)
+  Product.findById(req.params.id).then(product => {
+    if (product) {
+      res.status(200).json(product)
     } else {
-      res.status(404).json({message: 'Order not found!'});
+      res.status(404).json({message: 'Product not found!'});
     }
   });
 });
@@ -76,61 +80,5 @@ router.post("", checkAuth, async (req, res, next ) => {
       return res.send({ success: false, message: 'couldnt create order' })
     }
   })
-
-
-router.delete("/:id", checkAuth, async (req, res, next) => {
-  try {
-    const order = await global.models('Order').findById(req.params.orderId)
-    order.deleteOne()
-    res.json(await order)
-  } catch (e) {
-    res.send({ success: false, message: 'couldnt fetch order' })
-  }
-})
-
-router.get('/:id', checkAuth, async (req, res, next) => {
-  try {
-    const order = await global.models('Order').findById(req.params.orderId)
-    if(req.user.id !==  order.userId) return res.status(401).json({success: false, message: 'thats not your order'})
-    const user = await global.models('User').findById(order.userId)
-    res.json({
-       user, order
-    })
-  } catch (e) {
-    res.status(400).json({ success: false, message: 'couldnt fetch order' })
-  }
-})
-
-router.patch('/:id', checkAuth, async (req, res, next) => {
-  const orderId = req.params.orderId
-  if (!orderId) { return res.status(400).json({ success: false, message: 'your forgot to give the order id' }) }
-  try {
-      const order = await global.models('Order').findById(orderId)
-      await order.updateOne({
-          delivered: !order.delivered
-      })
-      await order.save()
-      res.json(await order)
-  } catch (e) {
-      console.log(e)
-      res.send({ success: false, message: 'couldnt update order' })
-  }
-})
-
-router.patch('/:id', checkAuth, async (req, res, next) => {
-  const orderId = req.params.orderId
-  if (!orderId) { return res.status(400).json({ success: false, message: 'your forgot to give the order id' }) }
-  try {
-      const order = await global.models('Order').findById(orderId)
-      await order.updateOne({
-              paid: !order.paid
-      })
-      await order.save()
-      res.json(await order)
-  } catch (e) {
-      console.log(e)
-      res.send({ success: false, message: 'couldnt update order' })
-  }
-})
 
 module.exports = router;
