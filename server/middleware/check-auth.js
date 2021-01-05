b0ca6319dd1047920d019f9cc421bf40722e0566
Nils van der Velden <nilsepils123@gmail.com>
2021-01-05 15:41:13 +0100

    Fixed expire time

--- server/middleware/check-auth.js
+++ server/middleware/check-auth.js
@@ -1,12 +1,18 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user")
 
-module.exports = (req, res, next) => {
+module.exports = async function (req, res, next) {
+  const token = req.header('token')
+  if (!token) return res.status(401).json({ succes: false, message: "couldn't authenticate" })
+
   try {
-    const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, "secret_this_should_be_longer");
-    next();
-  } catch (error) {
-    res.status(401).json({ message: "Auth failed!" });
+    const decoded = jwt.verify(token, process.env.JWT)
+    const user = await User.findById(decoded.user.id).catch(e=>{return res.status(500).send({ message: 'Invalid Token' })})
+    req.user = user
+    req.user.id = decoded.user.id
+    next()
+  } catch (e) {
+    // console.error(e)
+    res.status(500).send({ message: 'Invalid Token' })
   }
-};
+}
